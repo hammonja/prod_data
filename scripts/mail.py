49de27d0a080a188fcc6be4da29082921f783644
hammonja <james.hammond@bentham.co.uk>
2020-04-11 09:55:37 +0100

    removed mail , now uses js server that the production website uses

--- scripts/mail.py
+++
@@ -1,8 +0,0 @@
-import json
-
-
-#def send_message(_text, bom ):
-email = {"from": "accounts@bentham.co.uk","to": ["jameshammond@bentham.co.uk"],"bcc": ["david.hammond@bentham.co.uk"],"subject": "Test","html" : " some mesage ", "replyTo ": "jameshammond@bentham.co.uk"}
-json_email = json.dumps (email)
-
-print (json_email)
